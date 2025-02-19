776ea3c0de324a92fcab1c36d238cbdd74c4af24
Bravim <bravim.pedro@gmail.com>
2025-02-18 23:27:00 -0300

    add authenticate

--- src/routes/carDetailsRoutes.js
+++ src/routes/carDetailsRoutes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import CarDetailsController from '../controllers/carDetailsController.js';
+import { authenticate } from '../middlewares/authenticate.js';
 
 const router = express.Router();
 const carDetailsController = new CarDetailsController();
 
-router.get('/', (req, res) => carDetailsController.findAll(req, res));
-router.get('/:id', (req, res) => carDetailsController.findById(req, res));
+router.get('/', authenticate, (req, res) => carDetailsController.findAll(req, res));
+router.get('/:id', authenticate, (req, res) => carDetailsController.findById(req, res));
 
 // router.post('/', carDetailsController.create);
 // router.put('/:id', carDetailsController.update);
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -5,10 +5,10 @@ import {authenticate} from "../middlewares/authenticate.js"
 const router = express.Router();
 const userController = new UserController();
 
-router.get("/", (req, res) => userController.getAllUsers(req, res));  
+router.get("/", authenticate, (req, res) => userController.getAllUsers(req, res));  
 router.get("/:id", authenticate, (req, res) => userController.getUserById(req, res));  
 router.post("/register", (req, res) => userController.createUser(req, res));  
-router.put("/:id", (req, res) => userController.updateUser(req, res));
-router.delete("/:id", (req, res) => userController.deleteUser(req, res));
+router.put("/:id",authenticate,  (req, res) => userController.updateUser(req, res));
+router.delete("/:id", authenticate, (req, res) => userController.deleteUser(req, res));
 
 export default router;
